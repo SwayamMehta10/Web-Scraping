321e0e87d57e22dfffe2a15a24a560251675721a
SwayamMehta10 <79704715+SwayamMehta10@users.noreply.github.com>
2022-07-11 20:09:14 +0530

    unresolved mongod issue

--- StackOverflow/stack/stack/settings.py
+++ StackOverflow/stack/stack/settings.py
@@ -62,9 +62,12 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'stack.pipelines.StackPipeline': 300,
-#}
+ITEM_PIPELINES = {'stack.pipelines.MongoDBPipeline': 300,}
+
+MONGODB_SERVER = 'localhost'
+MONGODB_PORT = 27017
+MONGODB_DB = 'stackoverflow'
+MONGODB_COLLECTION = 'questions'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
